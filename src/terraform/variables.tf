33bef3000a866dc60c9c518d9a871bfbf5abd2ef
bachar <diamobachar06@gmail.com>
2024-12-11 01:28:55 +0100

    feat(CD): - add hashicorp terraform cloud support for a centralized state management           - integrate terraform cloud support into pipeline           - create a terraform destroy pipeline           - convert the aws region to a secret

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -2,3 +2,8 @@ variable "ec2_public_SSH_key" {
   description = "Public SSH key for EC2 access"
   type        = string
 }
+
+variable "aws_region" {
+  description = "aws region"
+  type        = string
+}
