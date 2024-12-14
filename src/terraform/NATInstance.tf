fdd2925c74e7fa261e4e434417fa0f5e4acc71f6
bachar <diamobachar06@gmail.com>
2024-12-14 15:42:40 +0100

    enhance(CD): Update OS and modify pipeline to support new Ansible setup-docker playbook
    + terraform: Changed instances os from amazon linux 2 to ubuntu and made appropriate changes for smoothless run
    + ansible: Set up ansible playbook "setup-docker" to update all machines and install docker, docker-compose
    + actions: Add re-usable "setup-ssh" actions
    + workflows: Create new updated version of "setup_machines" workflow (still on-going). Old one is deprecated. To be removed soon
    + ansible-inventory: List hosts, their users, and ssh tunneling setup for private subnet hosts

--- src/terraform/NATInstance.tf
+++ src/terraform/NATInstance.tf
@@ -1,10 +1,11 @@
 # NAT Instance using Amazon Linux 2 (Free tier eligible)
 resource "aws_instance" "nat_instance" {
-  ami           = "ami-02a0945ba27a488b7" # Using Amazon Linux 2 (free tier eligible)
+  ami           = "ami-075449515af5df0d1" # Ubuntu server 24.04 LTS AMI ID for eu-north-1
   instance_type = "t3.micro"
 
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.natSecGrp.id]
+  key_name               = aws_key_pair.my_key.key_name
 
   # Disable source/dest check for NAT
   source_dest_check = false
@@ -14,22 +15,26 @@ resource "aws_instance" "nat_instance" {
 
   user_data = <<-EOF
               #!/bin/bash
-
+              
               # Updating the system
-              sudo yum update -y
-
+              sudo apt-get update && sudo apt-get upgrade -y
+              
               # Installing iptables
-              sudo yum install iptables-services -y 
-
+              sudo apt-get install -y iptables-persistent
+              
               # Turning on IP Forwarding
               echo "net.ipv4.ip_forward = 1" | sudo tee -a /etc/sysctl.conf
               sudo sysctl -p
-
+              
               # Flush nat rules
-              iptables -t nat -F
+              sudo iptables -t nat -F
               
               # Making a catchall rule for routing and masking the private IP
-              sudo iptables -t nat -A POSTROUTING -o eth0 -s 10.0.1.0/24 -j MASQUERADE
+              sudo iptables -t nat -A POSTROUTING -o ens5 -s 10.0.1.0/24 -j MASQUERADE
+              
+              # Save iptables rules
+              sudo netfilter-persistent save
+              sudo netfilter-persistent reload
               EOF
 
   tags = {
