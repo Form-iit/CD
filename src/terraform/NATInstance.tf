e609b750872e47bb5dc33eed4f0924e97129edce
bachar <diamobachar06@gmail.com>
2024-12-19 13:47:44 +0100

    fix(terraform): changed package manager to apt instead of yum

--- src/terraform/NATInstance.tf
+++ src/terraform/NATInstance.tf
@@ -5,7 +5,8 @@ resource "aws_instance" "nat_instance" {
 
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.natSecGrp.id]
-
+  key_name               = aws_key_pair.my_key.key_name
+  
   # Disable source/dest check for NAT
   source_dest_check = false
 
@@ -14,22 +15,26 @@ resource "aws_instance" "nat_instance" {
 
   user_data = <<-EOF
               #!/bin/bash
-
+              
               # Updating the system
-              sudo yum update -y
-
+              sudo apt-get update && sudo apt-get upgrade -y
+              
               # Installing iptables
-              sudo yum install iptables-services -y 
-
+              sudo apt-get install -y iptables-persistent
+              
               # Turning on IP Forwarding
               echo "net.ipv4.ip_forward = 1" | sudo tee -a /etc/sysctl.conf
               sudo sysctl -p
-
+              
               # Flush nat rules
-              iptables -t nat -F
+              sudo iptables -t nat -F
               
               # Making a catchall rule for routing and masking the private IP
               sudo iptables -t nat -A POSTROUTING -o eth0 -s 10.0.1.0/24 -j MASQUERADE
+              
+              # Save iptables rules
+              sudo netfilter-persistent save
+              sudo netfilter-persistent reload
               EOF
 
   tags = {
